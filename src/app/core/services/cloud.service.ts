3dcdd0de30649775b21b6e52ed23260bc1eb145d
Mike Schauber <mike.schauber96@gmail.com>
2024-11-08 20:58:35 +0100

    connect project with firebase /firestore and implement listening function in CloudService

--- src/app/core/services/cloud.service.ts
+++ src/app/core/services/cloud.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, OnDestroy, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collection, doc, onSnapshot } from 'firebase/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CloudService {
+export class CloudService implements OnDestroy {
+  firestore: Firestore = inject(Firestore);
+  unsubChannels;
+  unsubMembers;
+  channels: any = [];
+  members: any = [];
 
-  constructor() { }
+  constructor() {
+    this.unsubChannels = this.subList('channels');
+    this.unsubMembers = this.subList('members');
+  }
+
+  ngOnDestroy(): void {
+    this.unsubChannels();
+    this.unsubMembers();
+  }
+
+  subList(ref: string) {
+    return onSnapshot(this.getRef(ref), (querySnapshot) => {
+      if (ref === 'channels') {
+        this.channels = querySnapshot.docs.map((doc) => doc.data());
+        console.log(this.channels);
+      } else if (ref === 'members') {
+        this.members = querySnapshot.docs.map((doc) => doc.data());
+        console.log(this.members);
+      }
+    });
+  }
+
+  getRef(ref: string) {
+    return collection(this.firestore, ref);
+  }
+
+  getSingleRef(ref: string, docId: string) {
+    return doc(collection(this.firestore, ref), docId);
+  }
 }
